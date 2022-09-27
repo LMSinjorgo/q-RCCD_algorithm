f7fba0213b3a96bf2c9b7b975cd509b0478c8d57
LMSinjorgo <94172274+LMSinjorgo@users.noreply.github.com>
2022-09-27 16:57:59 +0200

    Add files via upload

---
+++ DkS/qRCCA_DkS.m
@@ -0,0 +1,94 @@
+function [runningTime, objValue, x] = qRCCA_DkS(A,q,k,maxIter,T,eps)
+% Implementation of the qRCCA algorithm for 
+% the continuous relaxation of DkS (densest k subgraph problem).
+% Lennart Sinjorgo & Renata Sotirov
+
+% DkS:
+% minimize: x'Ax,
+% subject to: sum(x) = k, 0 \leq x \leq 1. Here, A is the adjaency matrix
+% of a graph
+
+% Each step, select q coordinates (uniformly random) to update.
+
+% For the QP:
+% max f(u)  = NablaF (u - xJ) - L/2 * || u - xJ ||^2
+%           = NablaF *u - L/2 * (u'u - 2u'x)      + Constants
+%           = - u' *(L/2)* u + (NablaF + L*x)'*u  + Constants
+%           = 0.5 u'*( -L*I )*u + c'u                  + Constants
+% s.t. aJ * uJ = aJ * xJ
+
+tic
+n = size(A,2);
+
+% initial feasible starting point
+x = (k/n) * ones(n,1);
+linA = A*x;
+quadForm = x'*linA;
+
+defaultopt = mskoptimset;
+options = mskoptimset(defaultopt,[]);
+[cmd,~,param] = msksetup(1,options);
+
+prob.qosubi = (1:q)';
+prob.qosubj = prob.qosubi;
+prob.blx    = zeros(1,q);
+prob.bux    = ones(1,q);
+prob.a = sparse(ones(1,q));
+
+onesVector = prob.bux';
+objOld = quadForm;
+
+
+currentIter = 1;
+
+% compute Lipschitz constant
+L = full(max(sum(A,2)));
+stagCounter = 1;
+while currentIter <  maxIter
+    J = randperm(n,q);
+    xJ = x(J);
+
+    % compute partial derivatives
+    NablaF = 2 * linA(J,:);
+    
+    % Set MOSEK parameters
+    sum_xJ = sum(xJ);
+    prob.blc    = sum_xJ;
+    prob.buc    = sum_xJ;
+    prob.qoval = L * onesVector;
+    prob.c = -(NablaF + L * xJ);
+    
+    % solve the quadratic programme
+    [~,res] = mosekopt(cmd,prob,param);
+    uJ = res.sol.itr.xx;
+    
+    % compute dJ and the next iterate of x
+    dJ = uJ - xJ;
+    x(J) = uJ;
+    
+    % update the variables tracking Ax, x'Ax, Bx, x'Bx
+    z_A = A(:,J)*dJ; 
+    
+    quadForm = quadForm + 2*dJ'*linA(J) + dJ'*z_A(J,:);
+    linA = linA + z_A;
+    
+    % track the objective increase
+    if (quadForm - objOld) < eps
+        stagCounter = stagCounter+1;
+        if stagCounter > T
+            break;
+        end
+    else
+        stagCounter = 0;
+    end
+    
+    objOld = quadForm;
+    
+    currentIter = currentIter+1;
+    
+end
+runningTime = toc;
+
+objValue = quadForm;
+end
+

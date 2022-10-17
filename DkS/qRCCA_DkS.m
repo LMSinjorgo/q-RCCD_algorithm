a960842066ce1de92c481ab1269ceb4893b9a5e5
LMSinjorgo <94172274+LMSinjorgo@users.noreply.github.com>
2022-10-17 16:06:14 +0200

    Update qRCCA_DkS.m

--- DkS/qRCCA_DkS.m
+++ DkS/qRCCA_DkS.m
@@ -1,22 +1,26 @@
-function [runningTime, objValue, x,m] = qRCCA_DkS(A,q,k,M,x)
+function [runningTime, objValue, x] = qRCCA_DkS(A,q,k,M,x,eps,maxStag)
 % Implementation of the qRCCA algorithm for
 % the continuous relaxation of DkS (densest k subgraph problem).
-% Lennart Sinjorgo & Renata Sotirov
-
-% DkS:
-% minimize: x'Ax,
-% subject to: sum(x) = k, 0 \leq x \leq 1. Here, A is the adjaency matrix
-% of a graph
-
-% Inputs:
+% Lennart Sinjorgo & Renata Sotirov, 2022
+%
+% INPUTS:
 % A: symmetric adjacency matrix of graph
 % q: number of coordinates to update simultaneously
 % k: number of vertices to select (appears in the constraint sum(x) = k )
 % M: maximum number of iterations (no other stopping criteria)
-% x: feasible starting vector.
-
-% outputs are obvious
-
+% x: feasible starting vector (Not necessary, can be left empty).
+% eps and maxStag: if sum(abs(x_k - x_{k+1}) < eps,
+%                  counter = counter + 1. If counter > maxStag, stop iterating
+%
+% Outputs are obvious.
+%
+% DkS:
+% minimize: x'Ax,
+% subject to: sum(x) = k, 0 \leq x \leq 1. Here, A is the adjaency matrix
+% of a graph
+%
+%
+%
 % In each step, we solve a QP for the update:
 % max f(u)  = NablaF (u - xJ) - L/2 * || u - xJ ||^2
 %           = NablaF *u - L/2 * (u'u - 2u'x)      + Constants
@@ -24,10 +28,22 @@ function [runningTime, objValue, x,m] = qRCCA_DkS(A,q,k,M,x)
 %           = 0.5 u'*( -L*I )*u + c'u                  + Constants
 % s.t. aJ * uJ = aJ * xJ
 
+
+% input checking
+if ~isempty(x)
+    if sum(x) ~= 1 || any(x > 1) || any(x < 0)
+        error("The provided x vector is not feasible!");
+    end
+end
+
+if ~issymmetric(A)
+    error("matrix A is not symmetric");
+end
+
 tic
 n = size(A,2);
 
-% initial feasible starting point (if no starting point is given by the
+% initialize feasible starting point (if no starting point is given by the
 % user)
 if isempty(x)
     x = (k/n) * ones(n,1);
@@ -51,7 +67,7 @@ G = [eye(q) - (1/q)*ones(q), (1/q)*ones(q,1)];
 
 % store zeroVector for speed
 zeroVector = zeros(q,1);
-
+stagnationCounter = 1;
 while m < M
     J = randperm(n,q);
     xJ = x(J);
@@ -83,19 +99,29 @@ while m < M
             % goals)
             % code taken from: 
             % https://github.com/canyilu/Projection-onto-the-capped-simplex
-            % MEX routine is probably faster
+            % We have also implemented the MEX version of function
+            % fullProj, but this led to no difference in speed.
             uJ = fullProj(uJ,sum_xJ,q,zeroVector);
         end
     end
 
     % compute dJ and the next iterate of x
     dJ = uJ - xJ;
+    
     x(J) = uJ;
 
     % update the variable tracking Ax
     linA = linA + A(:,J)*dJ;
 
     m = m+1;
+
+    % check stopping criteria
+    if sum(abs(dJ)) < eps
+        stagnationCounter = stagnationCounter+1;
+        if stagnationCounter > maxStag
+            break;
+        end
+    end
 end
 
 runningTime = toc;
